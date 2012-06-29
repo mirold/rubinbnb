d87b9c2af354a1a26ba60090665f0cdf3539aa03
mirold <mirold@gazeta.pl>
2012-06-29 13:24:59 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rubinbnb::Application.config.secret_token = 'f96c3e891f0ea32d5745fd1f0b3ef5f7cfae47f615056b6e213fb1a83400f5769dadcbab282428b9ef0c4914310c6f52ec09f17641df993aed8203b4d1728550'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Rubinbnb::Application.config.session_store :cookie_store, key: '_rubinbnb_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Rubinbnb::Application.config.session_store :active_record_store
